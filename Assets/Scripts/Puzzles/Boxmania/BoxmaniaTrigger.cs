0352856fcd43dc178848cf10366ae1b18be999a0
akimball6305 <aiden.kimball6305@gmail.com>
2025-01-26 11:41:12 -0600

    make Interact instructions
    when player touches trigger instructions are set to active

--- Assets/Scripts/Puzzles/Boxmania/BoxmaniaTrigger.cs
+++ Assets/Scripts/Puzzles/Boxmania/BoxmaniaTrigger.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class BoxmaniaTrigger : MonoBehaviour
 {
     [SerializeField] Boxmania BoxmaniaPuzzle;
+    [SerializeField] TextMeshProUGUI instructions;
 private void OnTriggerEnter(Collider trigger){
 
-    
-
-   
     BoxmaniaPuzzle.canBeActive = true;
-  
+    instructions.gameObject.SetActive(true);
 }
 
 private void OnTriggerExit(Collider trigger){
     BoxmaniaPuzzle.canBeActive = false;
-    
+    instructions.gameObject.SetActive(false);
 }
 
 }
